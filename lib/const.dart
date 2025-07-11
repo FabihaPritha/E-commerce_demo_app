097b4c6ffc2eb2622f0a54dba3d5988d0de54faa
Nowshin <ibnatnstu@gmail.com>
2025-07-11 11:28:05 +0600

    ..

--- lib/const.dart
+++
@@ -1,4 +0,0 @@
-const String stripePublishableKey = 
-  "pk_test_51RjHH5EQKwm4VFhJrxOJtGjeZ0EXAY4S4HBtXbKU1hpMAcf1I51xGn51YstQT1xIc2RnqiolF0Rif4csbPsOmof300xOMUzF0r";
-const String stripeSecretKey = 
-  "sk_test_51RjHH5EQKwm4VFhJhwSeA5pPHUxHfTc6ObbGVUKVY3ZesE5g3MhHDss14CCOsVzeLwv43PVflxLRxgxorTPjlSvE00F9TybQvP";
